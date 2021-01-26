cb5377aa469acea073e92a4a878a20012d570a92
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2021-01-26 20:45:01 +0200

    load teams current user is admin - at login

--- frontend/app/components/IndexLoggedIn.js
+++ frontend/app/components/IndexLoggedIn.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import {Link} from 'react-router-dom'
+import Axios from 'axios'
 
 import Page from './Page'
+import StateContext from '../StateContext'
 
 function IndexLoggedIn(props) {
+
+  const state = useContext(StateContext)
+  const [teams, setTeams] = useState([])
+
+  async function retrieveTeams() {
+    try {
+      const response = await Axios.get("http://localhost:3001/teams/my-teams", 
+                                       {
+                                         headers: {
+                                          'Content-Type': 'application/json',
+                                          'Authorization': 'Bearer ' + state.user.token
+                                         }
+                                       })
+      console.log(response.data)
+      setTeams(response.data)
+    } catch(e) {
+      console.log("Error retrieving teams.")
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    retrieveTeams()
+  }, [])
+
   return (
     <Page title="TeamVote"
           name="Home">
@@ -30,7 +57,19 @@ function IndexLoggedIn(props) {
           <div className="your-teams">
             <h1>Your teams</h1>
             <div className="teams-area">
-              
+              {
+                teams && teams.length > 0 && 
+                <>
+                  {
+                    teams.map(team => {
+                      return (
+                        <div className="team-name" key={team._id}>
+                          <a href="#">{team.name}</a>
+                        </div>
+                      )
+                    })}
+                </>
+              }
             </div>
           </div>
         </div>
