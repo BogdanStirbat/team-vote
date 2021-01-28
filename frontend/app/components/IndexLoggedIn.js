0812f36d02d41162f4c6b09a570e713e6ff8039a
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2021-01-28 16:29:07 +0200

    Search teams - frontend

--- frontend/app/components/IndexLoggedIn.js
+++ frontend/app/components/IndexLoggedIn.js
@@ -9,6 +9,29 @@ function IndexLoggedIn(props) {
 
   const state = useContext(StateContext)
   const [teams, setTeams] = useState([])
+  const [searchTeamName, setSearchTeamName] = useState()
+  const [searchTeamResults, setSearchTeamResults] = useState([])
+  const [noSearchResultsFound, setNoSearchResultsFound] = useState(false)
+
+  async function searchTeams() {
+    try {
+      const response = await Axios.get("http://localhost:3001/teams/search?q=" + searchTeamName, 
+                                      {
+                                        headers: {
+                                          'Content-Type': 'application/json',
+                                          'Authorization': 'Bearer ' + state.user.token
+                                        }
+                                      })
+      setSearchTeamResults(response.data)
+      console.log(response.data)
+      if (response.data.length == 0) {
+        setNoSearchResultsFound(true)
+      }
+    } catch(e) {
+      console.log("Error searching teams.")
+      console.log(e)
+    }
+  }
 
   async function retrieveTeams() {
     try {
@@ -24,6 +47,12 @@ function IndexLoggedIn(props) {
       console.log("Error retrieving teams.")
       console.log(e)
     }
+  }
+
+  function onSearchFieldChange(e) {
+    setSearchTeamName(e.target.value)
+    setNoSearchResultsFound(false)
+    setSearchTeamResults([])
   }
 
   useEffect(() => {
@@ -47,9 +76,27 @@ function IndexLoggedIn(props) {
             <p>Join an existing team</p>
 
             <div className="search-team">
-              <input type="text" placeholder="Search.." name="search" />
-              <button type="submit"><i className="fa fa-search"></i></button>
+              <input onChange={onSearchFieldChange} type="text" placeholder="Search.." name="search" />
+              <button onClick={searchTeams} type="submit"><i className="fa fa-search"></i></button>
             </div>
+
+            { 
+              searchTeamResults && searchTeamResults.length > 0 &&
+                <div className="search-results">
+                  {
+                    searchTeamResults.map(team => {
+                      return <Link to={"/team/" + team._id}>{team.name}</Link>
+                    })
+                  }
+                </div>
+            }
+
+            {
+              noSearchResultsFound &&
+                <div className="no-search-results">
+                  No search results was found.
+                </div>
+            }
 
           </div>
 
